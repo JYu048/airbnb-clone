545b43dfb29690f8cd2c7a6c6270dd043698bd67
Jerry Yu <JaredY3325@Gmail.com>
2023-08-09 16:59:06 -0600

    Added header and login pages

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function RegisterPage() {
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto">
+          <input type="text" placeholder="your@email.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            <span className="mr-1">Already a member?</span>
+            <Link to={"/login"} className="underline text-black">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterPage;
